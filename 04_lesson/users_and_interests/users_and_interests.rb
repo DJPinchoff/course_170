6e6781efde2c7e55562e0ac984fb9d9a8883f9be
Dave Pinchoff <dpinchoff@gmail.com>
2017-07-14 12:13:37 -0400

    Completed Code Challenge: Users and Interests

--- 04_lesson/users_and_interests/users_and_interests.rb
+++ 04_lesson/users_and_interests/users_and_interests.rb
@@ -3,12 +3,43 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require 'yaml'
 
+before do
+  @database = Psych.load_file("users.yml")
+  @number_of_users = @database.keys.size
+end
+
 get "/" do
   redirect "/users"
 end
 
 get "/users" do
-  @database = Psych.load_file("users.yml")
-  
+  @title = "User Directory"
   erb :users
+end
+
+get "/users/:name" do
+  @name = params[:name]
+  @title = "#{@name.capitalize}'s Page"
+  erb :user
+end
+
+helpers do
+  def other_users(name)
+    result = []
+    @database.each_key do |key|
+      result << key unless key == name
+    end
+    result
+  end
+
+  def count_interests
+    count = 0
+    @database.each do |key, value|
+      value[:interests].each do |_|
+        count += 1
+      end
+    end
+
+    count
+  end
 end
