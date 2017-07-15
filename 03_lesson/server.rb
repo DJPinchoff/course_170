05aeac21a962839740240ba477b484f0b0dc1631
Dave Pinchoff <dpinchoff@gmail.com>
2017-07-15 13:58:53 -0400

    Final Sinatra Todo Example from LS added

---
+++ 03_lesson/server.rb
@@ -0,0 +1,43 @@
+require "socket"
+
+def parse_request(request_line)
+  method, path_query, _ = request_line.split(" ")
+  path, query = path_query.split("?")
+  params = {}
+  query.split("&").each do |item|
+    param_value = item.split("=")
+    params[param_value[0]] = param_value[1]
+  end
+
+  [method, path, params]
+end
+
+server = TCPServer.new("localhost", 3003)
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
+  puts request_line
+
+  http_method, path, params = parse_request(request_line)
+
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/html\r\n\r\n"
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
+
+  client.puts "<h1>Rolls!</h1>"
+  params["rolls"].to_i.times do
+    client.puts "<p>", rand(params["sides"].to_i) + 1, "</p>"
+  end
+
+  client.puts "</body>"
+  client.puts "</html>"
+  client.close
+end
